5456f1da9b118daf64e2549836565b4c7ea8b93c
Mariasina <carolbeveer@gmail.com>
2024-09-20 11:56:02 -0300

    working on navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,104 @@
-export default function Navbar() {
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { MenuLink } from './styles';
+import { MarcellusFont } from '../GlobalFonts';
+
+function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <></>
-    )
+        <AppBar position="static">
+            <Container>
+                <Toolbar disableGutters>
+
+                    LOGO
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+
+                        <IconButton
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            <MenuLink>
+                                <MarcellusFont>
+                                    Home
+                                </MarcellusFont>
+                            </MenuLink>
+                            <MenuLink>
+                                <MarcellusFont>
+                                    Cardápios
+                                </MarcellusFont>
+                            </MenuLink>
+                            <MenuLink>
+                                Usuários
+                            </MenuLink>
+                        </Menu>
+
+
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        <MenuLink>
+                            Home
+                        </MenuLink>
+                        <MenuLink>
+                            Cardápios
+                        </MenuLink>
+                        <MenuLink>
+                            Usuários
+                        </MenuLink>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
+export default NavBar;
