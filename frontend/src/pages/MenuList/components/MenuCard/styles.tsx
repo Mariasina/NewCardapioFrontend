c191dc990a40f61599ae041bfb6efc409edf8df5
akda007 <akdagmr007s@gmail.com>
2024-09-23 11:44:01 -0300

    Almost finished the menu list page

---
+++ frontend/src/pages/MenuList/components/MenuCard/styles.tsx
@@ -0,0 +1,20 @@
+import { Box, styled } from "@mui/material";
+
+export const CardBox = styled(Box)(({}) => ({
+    outline: "4px solid var(--bg-primary)",
+    width: "190px",
+    aspectRatio: "5/3",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "20px",
+    margin: "auto 0",
+    justifySelf: "center",
+    transition: "300ms",
+    boxShadow: "none",
+
+    ":hover": {
+        transform: "scale(1.05)",
+        boxShadow: "3px 3px 15px 5px #80808082"
+    }
+}))
--- frontend/src/pages/MenuList/index.tsx
+++ frontend/src/pages/MenuList/index.tsx
@@ -1,22 +1,48 @@
 import { Box, Fab, Stack, Typography } from "@mui/material";
 import NavBar from "../../components/NavBar";
 import "./styles.css"
-import { ListTitleContainer } from "./styles";
+import { CardContainer, ListTitleContainer } from "./styles";
+import MenuCard from "./components/MenuCard";
 
 export default function MenuList() {
     return (
         <>
             <NavBar/>
-            <Stack bgcolor="#F8F4EB" padding={10}>
-                <Stack flexDirection={"row"} justifyContent={"space-between"}>
-                    <ListTitleContainer>
-                        <Typography>Registered Menu List</Typography>
-                    </ListTitleContainer>
-                    <Stack flexDirection={"row"} alignItems={"center"} justifyContent={"center"} gap={3}>
-                        <Typography>Add new Menu</Typography>
-                        <Fab color="inherit" aria-label="add">
-                            Add
-                        </Fab>
+            <Stack alignItems={"center"} justifyContent={"center"} width={"100%"} pt={10}>
+                <Stack bgcolor="#F8F4EB" sx={{width: "90%", maxWidth: "1000px"}}>
+                    <Stack flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"} sx={{
+                        "@media screen and (max-width: 425px)": {
+                            flexDirection: "column-reverse",
+                            gap: "30px"
+                        }
+                    }}>
+                        <ListTitleContainer>
+                            <Typography>Registered Menu List</Typography>
+                        </ListTitleContainer>
+                        <Stack flexDirection={"row"} alignItems={"center"} justifyContent={"center"} gap={2}>
+                            <Typography>Add new Menu</Typography>
+                            <Fab color="inherit" sx={{
+                                backgroundColor:"#CCA67F",
+
+                                ":hover": {
+                                    backgroundColor:"#dec4ab",
+                                }
+
+                            }} aria-label="add">
+                                <span className="material-symbols-outlined">add</span>
+                            </Fab>
+                        </Stack>
+                    </Stack>
+                    <Stack alignItems={"center"} justifyContent={"center"}>
+                        <CardContainer>
+                            <MenuCard/>
+                            <MenuCard/>
+                            <MenuCard/>
+                            <MenuCard/>
+                            <MenuCard/>
+                            <MenuCard/>
+                            <MenuCard/>
+                        </CardContainer>
                     </Stack>
                 </Stack>
             </Stack>
