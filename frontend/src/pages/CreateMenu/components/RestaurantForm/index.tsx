357c8f8b31a3718cf9148e60a8340aac8ba5664b
akda007 <akdagmr007s@gmail.com>
2024-09-25 13:27:54 -0300

    work-in-progress: Create Menu page

--- frontend/src/pages/CreateMenu/components/RestaurantForm/index.tsx
+++ frontend/src/pages/CreateMenu/components/RestaurantForm/index.tsx
@@ -1,16 +1,52 @@
-import { Stack } from "@mui/material";
+import { Fab, Stack } from "@mui/material";
 import { DescriptionInput, TitleInput, TitleInputContainer } from "./styles";
-import { useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-export default function RestaurantForm() {
-    const [description, setDescription] = useState("Descrição do restaurante")
+type IngredientsFieldProps = {
+    ingredients: Ingredient[],
+    setIngredients: React.Dispatch<React.SetStateAction<Ingredient[]>>
+}
+
+export type Ingredient = {
+    name: string,
+    hasGluten: boolean,
+    isAnimal: boolean,
+    isMeat: boolean
+}
+
+
+
+const IngredientsField = ({ingredients}: IngredientsFieldProps) => {
     return (
         <>
             <Stack>
+                {ingredients.map(x => <>
+                    
+                </>)}
+                
+                <Fab>
+                    <span className="material-symbols-outlined">add</span>
+                </Fab>
+            </Stack>
+        </>
+    )
+}
+
+export default function RestaurantForm() {
+    const [title, setTitle] = useState("Nome do restaurante")
+    const [description, setDescription] = useState("Descrição do restaurante")
+    const [ingredients, setIngredients] = useState<Ingredient[]>([])
+
+
+    return (
+        <>
+            <Stack gap={2}>
                 <TitleInputContainer>
-                    <TitleInput/>
+                    <TitleInput value={title} onChange={(e) => {setTitle(e.target.value)}}/>
                 </TitleInputContainer>
                 <DescriptionInput rows={3} value={description} onChange={(e) => {setDescription(e.target.value)}}/>
+
+                <IngredientsField ingredients={ingredients}/>
             </Stack>
         </>
     )
